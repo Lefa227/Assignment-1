d8de035dfc25d7e0b41005c9e06183554850be26
RC_Student_lab <RC_Student_lab@10.0.1.2>
2024-09-30 16:38:22 +0000

    First Project commit

---
+++ src/main/java/com/mycompany/lefapart1poe/LefaPart1POE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lefapart1poe;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LefaPart1POE {
+
+    public static void main(String[] args) {
+       
+    }
+}
