5f64d9220fc4140f4dca97f50adb5cc08b25da96
RC_Student_lab <RC_Student_lab@10.0.1.2>
2024-09-30 17:03:38 +0000

    First Project commit No.2

--- src/main/java/com/mycompany/lefapart1poe/LefaPart1POE.java
+++ src/main/java/com/mycompany/lefapart1poe/LefaPart1POE.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lefapart1poe;
 
+import java.util.Scanner;
+
 /**
  *
  * @author RC_Student_lab
@@ -12,5 +14,27 @@ public class LefaPart1POE {
 
     public static void main(String[] args) {
        
-    }
+        //Call the methods from the external class 
+         Login log = new Login();
+         
+         //Import scanner 
+          Scanner scanner = new Scanner(System.in);
+          
+           System.out.print("Please enter your firstname:");
+        String firstname = scanner.nextLine();
+        
+        System.out.print("Please enter your lastname:");
+        String lastname = scanner.nextLine();
+        String username;
+        String password;
+        
+        do{
+        System.out.print("Please enter your username:");
+         username = scanner.nextLine();
+    }while(!log.checkUsername(username));
+        
+        do{
+        System.out.print("Please enter your password:");
+        password = scanner.nextLine();
+    }while(log.(password));
 }
--- src/main/java/com/mycompany/lefapart1poe/Login.java
+++ src/main/java/com/mycompany/lefapart1poe/Login.java
@@ -10,4 +10,40 @@ package com.mycompany.lefapart1poe;
  */
 public class Login {
     
+    
+    String enteredUsername; // Entered username during login
+    String enteredPassword; // Entered password during login
+    String firstName; // User's first name
+    String lastName; // User's last name
+    String password; // User's password
+    String username; // User's username
+
+    // Check if the username is valid
+    public boolean checkUsername() {
+        if (username.length() <= 5 && username.contains("_")) {
+            return true;
+        }
+        return false;
+    }
+    
+    
+    // Check if the password meets complexity requirements
+    public boolean checkPasswordComplexity() {
+        boolean hasCapitalLetter = false;
+        boolean hasNumber = false;
+        boolean hasSpecialChar = false;
+
+        if (password.length() >= 8) {
+            for (char ch : password.toCharArray()) {
+                if (Character.isUpperCase(ch)) {
+                    hasCapitalLetter = true;
+                } else if (Character.isDigit(ch)) {
+                    hasNumber = true;
+                } else if (!Character.isLetterOrDigit(ch)) {
+                    hasSpecialChar = true;
+                }
+            }
+        }
+        return hasCapitalLetter && hasNumber && hasSpecialChar;
+    }
 }
